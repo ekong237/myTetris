aa661cd4a485b38aa8956cf6bbacabbef52ee2d5
ekong237 <liz.kong@yahoo.com>
2018-01-03 19:42:07 -0800

    merges dom piece to arena table

--- tetris.js
+++ tetris.js
@@ -10,6 +10,14 @@ const matrix = [
   [0, 1, 0]
 ];
 
+function createMatrix(w, h) {
+  const matrix = [];
+  while (h--) {
+    matrix.push(new Array(w).fill(0))
+  }
+  return matrix;
+}
+
 function draw() {
   context.fillStyle = '#000';
   context.fillRect(0, 0, canvas.width, canvas.height);
@@ -29,6 +37,16 @@ function drawMatrix(matrix, offset) {
   });
 }
 
+function merge(arena, player) {
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        arena[y + player.position.y][x + player.position.x] = value;
+      }
+    })
+  })
+}
+
 let dropCounter = 0;
 let dropInterval = 1000; // every one sec drop the piece by one step
 
@@ -36,9 +54,7 @@ let lastTime = 0;
 function update(time = 0) { 
   const deltaTime = time - lastTime;
   lastTime = time;
-  console.log('lasttime', lastTime);
   dropCounter += deltaTime;
-  console.log('dropcounter:', dropCounter);
   if (dropCounter > dropInterval) {
     player.position.y++;
     dropCounter = 0;
@@ -47,10 +63,27 @@ function update(time = 0) {
   requestAnimationFrame(update)
 }
 
+const arena = createMatrix(12, 20);
+console.log(arena);
+console.table(arena);
+
 const player = {
   position: {x: 5, y: 5},
   matrix: matrix
 }
+
+document.addEventListener('keydown', event => {
+  if (event.keyCode === 37) { //left
+    player.position.x--;
+  } else if (event.keyCode === 39) {
+    player.position.x++;
+  } else if (event.keyCode === 40) {
+    player.position.y++;
+    dropCounter = 0;
+  }
+});
+
+
 
 update();
 
